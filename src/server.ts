00847467ac38c1fac13de51983e6cabc7dd8d783
Andrei Carapcea <Andrei.Carapcea@softwire.com>
2021-06-30 15:03:15 +0300

    Done part 2, get rovers

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,67 @@
-import express from "express";
+import express from 'express';
+import { readFileSync } from 'fs';
+import axios, {AxiosResponse} from 'axios';
+
+class MarsApp {
+    
+    app = express();
+    port = 8000;
+    router = express.Router();
+
+    // path to the NASA API key
+    KEY_PATH = 'res/key';
+
+    key : string = "";
+    
+    ReadAPIKey(): string {
+        let ret : string = readFileSync(this.KEY_PATH).toString();
+        return ret;
+    }
+
+    /*
+     * Returns the list of rovers in JSON format.
+     * Sends a GET request to api.nasa.gov.
+     * Requires an API key.
+     */
+    async GetRoverList(key: string) : Promise<string> {
+        const roverPath = 'https://api.nasa.gov/mars-photos/api/v1/rovers' + '?api_key=' + key;
+        let ret : string = "";
+
+        try {
+            const response = await axios.get(roverPath);
+            ret = response.data;
+        } catch (exception) {
+            console.error(exception);
+        }
+
+        return ret;
+    }
+
+    /*
+     * Setup /rovers path for the server.
+     */
+    SetupRoversPath(key: string) : void {
+        this.router.get('/rovers', (req, res) => {
+            this.GetRoverList(key)
+                .then(rovers => res.send(rovers))
+                .catch(err => console.log(err));
+        });
+    }
+
+    constructor() {
+        this.app.use(express.json());
+
+        this.key = this.ReadAPIKey();
  
-const app = express();
-const port = 8000;
- 
-app.use(express.json());
-const router = express.Router();
-router.get('/test', (req, res) => res.send('Hello world !'));
-app.use('/', router);
- 
-app.listen(port, () => {
-  console.log(`Test backend is running on port ${port}`);
-});
+        this.SetupRoversPath(this.key);
+    
+        this.app.use('/', this.router);
+         
+        this.app.listen(this.port, () => {
+            console.log(`Test backend is running on port ${this.port}`);
+        });
+    }
+}
+
+
+let  app : MarsApp = new MarsApp();
