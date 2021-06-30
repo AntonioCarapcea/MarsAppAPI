dbdc849d129d478635d7135cc5f1228eb619d1f6
Andrei Carapcea <Andrei.Carapcea@softwire.com>
2021-06-30 16:53:29 +0300

    Implemented the suggestions

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { readFileSync } from 'fs';
 import axios, {AxiosResponse} from 'axios';
+import {nasaAPIKey} from './key'
 
 class MarsApp {
     
@@ -8,52 +9,34 @@ class MarsApp {
     port = 8000;
     router = express.Router();
 
-    // path to the NASA API key
-    KEY_PATH = 'res/key';
-
-    key : string = "";
-    
-    ReadAPIKey(): string {
-        let ret : string = readFileSync(this.KEY_PATH).toString();
-        return ret;
-    }
+    key = nasaAPIKey;
 
     /*
      * Returns the list of rovers in JSON format.
      * Sends a GET request to api.nasa.gov.
      * Requires an API key.
      */
-    async GetRoverList(key: string) : Promise<string> {
-        const roverPath = 'https://api.nasa.gov/mars-photos/api/v1/rovers' + '?api_key=' + key;
-        let ret : string = "";
+    GetRovers(key: string) {
+        const path = 'https://api.nasa.gov/mars-photos/api/v1/rovers' + '?api_key=' + key;
 
-        try {
-            const response = await axios.get(roverPath);
-            ret = response.data;
-        } catch (exception) {
-            console.error(exception);
-        }
-
-        return ret;
+        return axios.get(path);
     }
 
     /*
-     * Setup /rovers path for the server.
+     * Setup /rovers endpoint for the server.
      */
-    SetupRoversPath(key: string) : void {
+    SetupRoversEndpoint(key: string) : void {
         this.router.get('/rovers', (req, res) => {
-            this.GetRoverList(key)
-                .then(rovers => res.send(rovers))
+            this.GetRovers(key)
+                .then(rovers => res.send(rovers.data))
                 .catch(err => console.log(err));
         });
     }
 
     constructor() {
         this.app.use(express.json());
-
-        this.key = this.ReadAPIKey();
  
-        this.SetupRoversPath(this.key);
+        this.SetupRoversEndpoint(this.key);
     
         this.app.use('/', this.router);
          
