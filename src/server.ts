773f6e4b901302d806717b37c96e9b5f90266197
Andrei Carapcea <Andrei.Carapcea@softwire.com>
2021-07-02 12:01:02 +0300

    Added cross origin resource sharing on localhost:3000

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { nasaAPIKey } from './key'
 import router from './router'
+import cors from 'cors'
 
 class MarsApp {    
     app = express();
@@ -9,9 +10,15 @@ class MarsApp {
     key = nasaAPIKey;    
     
     constructor() {
+        this.app.use(cors({
+            origin: 'http://localhost:3000'
+        }));
+        
         this.app.use(express.json());
 
         this.app.use('/', router);
+
+        
          
         this.app.listen(this.port, () => {
             console.log(`Test backend is running on port ${this.port}`);
